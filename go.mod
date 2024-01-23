5bc27663afd59a927ff66059bbe61e60970b548b
putragabrielll <putragmahk@gmail.com>
2024-01-23 11:41:17 +0700

    first commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/putragabrielll/go-introduction-2
+
+go 1.21.3
---
+++ main.go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("sudah berhasil dari MAIN!")
+
+	Pembulatan()
+}
