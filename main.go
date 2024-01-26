9ae9d5fb3fc856af908b3033c406ebfb1ebb38db
putragabrielll <putragmahk@gmail.com>
2024-01-26 08:41:04 +0700

    fix: task 2

--- main.go
+++ main.go
@@ -1,9 +1,6 @@
 package main
 
-import "fmt"
-
 func main() {
 	// Pembulatan(4.37)
-	fmt.Println("")
 	Cetak(40)
 }
--- task2.go
+++ task2.go
@@ -2,51 +2,67 @@ package main
 
 import "fmt"
 
-// import "fmt"
-
 type bilangan struct {
-	prima int
-	ganjil int
-	genap int
+	prima []int
+	ganjil []int
+	genap []int
 	fibonacci int
 }
 
-func (temp_1 bilangan) deretanBilangan() int{
-	return temp_1.genap
+func isPrime(n int) bool {
+    if n <= 1 {
+        return false
+    }
+    for i := 2; i*i <= n; i++ {
+        if n % i == 0 {
+            return false
+        }
+    }
+    return true
 }
 
+func (temp_1 bilangan) deretanPrima() []int{
+	return temp_1.prima
+}
+func (temp_1 bilangan) bilanganGanjil() []int{
+	return temp_1.ganjil
+}
+func (temp_1 bilangan) bilanganGenap() []int{
+	return temp_1.genap
+}
+// func (temp_1 bilangan) deretanBilangan() []int{
+// 	return temp_1.ganjil
+// }
+
+
 func Cetak(angka int){
-	var temp_genap int
-	var temp_ganjil int
-	temp_genap = 0
-	temp_ganjil = 0
-	for i := 0; i <= angka; i++ {
+	temp_prima := []int{}
+	temp_ganjil := []int{}
+	temp_genap := []int{}
+	// temp_fibonacci := []int{}
+
+	for i := 1; i <= angka; i++ {
 		if i % 2 == 0 {
 			// fmt.Println(i)
-			temp_genap+=i
+			temp_genap = append(temp_genap, i)
 		} else if i % 2 == 1 {
 			// fmt.Println(i)
-			temp_ganjil+=i
+			temp_ganjil = append(temp_ganjil, i)
 		}
+		if isPrime(i) {
+            temp_prima = append(temp_prima, i)
+        }
 	}
 
 	var data bilangan = bilangan{
-		prima: 1,
+		prima: temp_prima,
 		ganjil: temp_ganjil,
 		genap: temp_genap,
 		fibonacci: 12,
 	}
 
-	fmt.Println(data.deretanBilangan())
-
-
-
-	// MAP CONTOH
-	// biodata := map[string]string{
-	// 	"name": "gabriel",
-	// 	"pendidikan": "UNAI",
-
-	// }
-
-	// fmt.Println((biodata["pendidikan"]))
+	fmt.Println(data.deretanPrima())
+	fmt.Println(data.bilanganGanjil())
+	fmt.Println(data.bilanganGenap())
+	// fmt.Println(data.bilanganFibonacci())
 }
