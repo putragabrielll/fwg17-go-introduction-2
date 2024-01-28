20fb25b7862e591fb75c44212ed49bf49daf00cf
putragabrielll <putragmahk@gmail.com>
2024-01-28 13:24:29 +0700

    fix: commant

--- main.go
+++ main.go
@@ -5,14 +5,16 @@ import "fmt"
 func main() {
 	fmt.Println("Welcome To My New Website!")
 
+	// Task 1
 	// Pembulatan(4.37)
 	// fmt.Println("")
 	// Pembulatan(4.32)
 	// fmt.Println("")
 	// Pembulatan(4.324)
 	
+	// Task 2
 	// Cetak(40)
 
-	// fmt.Println(CekKubus(25))
-	CekKubus(5)
+	// task 3
+	// CekKubus(5)
 }
